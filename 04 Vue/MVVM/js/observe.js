1ff9b91915aee22b149860ec148942ae83543a5c
hyfun <775183940@qq.com>
2021-01-27 11:31:37 +0800

    fix: 优化dep

--- 04 Vue/MVVM/js/observe.js
+++ 04 Vue/MVVM/js/observe.js
@@ -3,12 +3,11 @@
  * @Date         : 2021-01-26 18:28:53
  * @Description  :
  * @LastEditors  : HyFun
- * @LastEditTime : 2021-01-26 19:40:45
+ * @LastEditTime : 2021-01-27 11:30:33
  */
 class Observe {
   constructor(data) {
     this.data = data;
-    this.dep = new Dep()
     if (typeof this.data === "object") {
       this.observe(this.data);
     }
@@ -23,18 +22,18 @@ class Observe {
   }
 
   defineReactive(target, key, value) {
-    const that = this
+    const dep = new Dep()
     Object.defineProperty(target, key, {
       configurable: true,
       enumerable: true,
       get() {
-        Dep.target && that.dep.addSub(Dep.target)
+        Dep.target && dep.addSub(Dep.target)
         return value;
       },
       set(newVal) {
         if (newVal != value) {
           value = newVal;
-          that.dep.notify()
+          dep.notify()
         }
       },
     });

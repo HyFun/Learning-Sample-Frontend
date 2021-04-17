f74b33a10122f2a50ba18b062c861ef989ab7d11
hyfun <775183940@qq.com>
2021-04-17 17:47:20 +0800

    feat: ts-snake游戏搭建完成

---
+++ TypeScript/part5/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author       : HyFun
+ * @Date         : 2021-04-17 17:26:06
+ * @Description  : 
+ * @LastEditors  : HyFun
+ * @LastEditTime : 2021-04-17 17:34:31
+ */
+import './index.less'
+console.log(111);
---
+++ TypeScript/part5/webpack.config.js
@@ -0,0 +1,92 @@
+/*
+ * @Author       : HyFun
+ * @Date         : 2021-04-10 11:54:18
+ * @Description  :
+ * @LastEditors  : HyFun
+ * @LastEditTime : 2021-04-17 17:44:11
+ */
+
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    environment: {
+      arrowFunction: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          // 配置babel-loader
+          {
+            // 加载器
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  // 指定环境的插件
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      chrome: "88",
+                      ie: "11"
+                    },
+                    // 指定corejs版本
+                    corejs: "3",
+                    // 使用corejs的方法
+                    //    usage : 按需加载
+                    useBuiltIns: 'usage'
+                  },
+                ],
+              ],
+            },
+          },
+          "ts-loader",
+        ],
+        exclude: /node_modules/,
+      },
+      // 设置less文件处理
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          // 引入postcss
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      browsers: 'last 2 versions'
+                    }
+                  ]
+                ]
+              }
+            }
+          },
+          'less-loader',
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      title: "typescript 学习",
+    }),
+  ],
+  // 用来设置引用模块
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};

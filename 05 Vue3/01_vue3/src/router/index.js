4061ed64a30c6ba4c622123e343d14584498f8a6
hyfun <775183940@qq.com>
2021-04-30 13:42:57 +0800

    feat: 暂存

--- 05 Vue3/01_vue3/src/router/index.js
+++ 05 Vue3/01_vue3/src/router/index.js
@@ -3,7 +3,7 @@
  * @Date         : 2021-04-21 22:09:08
  * @Description  :
  * @LastEditors  : heyongfeng
- * @LastEditTime : 2021-04-26 22:14:24
+ * @LastEditTime : 2021-04-30 12:27:45
  */
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '../App.vue'
@@ -67,6 +67,13 @@ export const routes = [
         component: () => import('../views/lesson7/index.vue'),
         meta: {
           title: '课时七：全局属性'
+        }
+      },
+      {
+        path: '/lesson8',
+        component: () => import('../views/lesson8/index.vue'),
+        meta: {
+          title: '课时八：Composition API'
         }
       }
     ]
---
+++ TypeScript/002/part3/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author       : HyFun
+ * @Date         : 2021-04-30 13:20:35
+ * @Description  : 
+ * @LastEditors  : HyFun
+ * @LastEditTime : 2021-04-30 13:21:10
+ */
+
+document.write(`哈哈哈哈~`)

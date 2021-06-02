69abf135385f3a1a64412449dd2d765eeb4a172c
hyfun <775183940@qq.com>
2021-06-02 19:34:12 +0800

    chore: update vueconfig

---
+++ 05 Vue3/01_componsition-api/vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author       : heyongfeng
+ * @Date         : 2021-06-02 10:20:19
+ * @Description  : vue配置文件
+ * @LastEditors  : heyongfeng
+ * @LastEditTime : 2021-06-02 10:22:35
+ */
+
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 9001,
+    open: true
+  }
+}

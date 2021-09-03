d5c4bc4ab373ca9d2a2dc03d96128c3685e20827
hyfun <775183940@qq.com>
2021-09-03 18:47:00 +0800

    feat: update

--- React/002-staging/002_setProxy/setupProxy.js
+++ React/002-staging/002_setProxy/setupProxy.js
@@ -3,13 +3,13 @@
  * @Date         : 2021-09-01 22:48:40
  * @Description  : react中间件代理
  * @LastEditors  : HyFun
- * @LastEditTime : 2021-09-01 22:54:25
+ * @LastEditTime : 2021-09-03 18:00:16
  */
 const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(
     proxy('/api1', {
-      target: '',
+      target: 'http://10.123:80',
       changeOrigin: true,
       pathRewrite: {
         '^/api1': ''

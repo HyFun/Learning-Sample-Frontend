8c388664dd3030fa2a981c048472b2b576772d3c
hyfun <775183940@qq.com>
2021-09-18 17:18:44 +0800

    chore: update

---
+++ React/005-redux/src/redux/store.js
@@ -0,0 +1,12 @@
+/*
+ * @Author       : heyongfeng
+ * @Date         : 2021-09-18 15:24:11
+ * @Description  :
+ * @LastEditors  : heyongfeng
+ * @LastEditTime : 2021-09-18 17:18:42
+ */
+import { createStore } from 'redux'
+
+
+
+export default createStore()

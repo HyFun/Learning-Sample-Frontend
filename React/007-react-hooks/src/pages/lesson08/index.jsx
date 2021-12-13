3b233070684eb036cb7a0043bd516e8cb34938e5
TD-heyongfeng <yongfeng.he@china.talkdesk.com>
2021-12-13 18:41:37 +0800

    chore: update

--- React/007-react-hooks/src/pages/lesson08/index.jsx
+++ React/007-react-hooks/src/pages/lesson08/index.jsx
@@ -2,12 +2,12 @@ import React, { useCallback, useState } from "react";
 
 export default function () {
   const [count, setCount] = useState(0);
-  const computedCountFn = useCallback(() => (() => count * 2), [count]);
+  const computedCountFn = useCallback(() => count * 2, [count]);
   return (
     <div>
       <h3>课时8：useCallback使用</h3>
       <h4>count: {count}</h4>
-      <h4>computedCountFn: {computedCountFn()()}</h4>
+      <h4>computedCountFn: {computedCountFn()}</h4>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );

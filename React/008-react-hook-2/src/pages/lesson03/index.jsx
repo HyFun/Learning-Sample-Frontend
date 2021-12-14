eb75d2aa303372da2161058d0152bb2416cf21fb
TD-heyongfeng <yongfeng.he@china.talkdesk.com>
2021-12-14 18:51:50 +0800

    chore: update

--- React/008-react-hook-2/src/pages/lesson03/index.jsx
+++ React/008-react-hook-2/src/pages/lesson03/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 // components
 import Sub from "./Sub";
@@ -6,8 +6,15 @@ import SubInput from "./SubInput";
 
 export default function Ref() {
   const inputRef = useRef(null);
-  const subRef = useRef(null)
-  const subinputRef = useRef(null)
+  const subRef = useRef(null);
+  const subinputRef = useRef(null);
+
+  const [count, setCount] = useState(0);
+  const stateRef = useRef(null);
+
+  useEffect(() => {
+    stateRef.current = count;
+  });
 
   useEffect(() => {
     inputRef.current.focus();
@@ -19,10 +26,20 @@ export default function Ref() {
       <input ref={inputRef} type="text" />
       <h4>2. ref获取子组件暴露的方法</h4>
       <Sub ref={subRef}></Sub>
-      <button onClick={()=>{subRef.current.show()}}>click</button>
+      <button
+        onClick={() => {
+          subRef.current.show();
+        }}
+      >
+        click
+      </button>
       <h4>3. ref操作子组件中的dom</h4>
-      <SubInput ref={subinputRef}/>
-      <button onClick={()=>alert(subinputRef.current.value)}>click</button>
+      <SubInput ref={subinputRef} />
+      <button onClick={() => alert(subinputRef.current.value)}>click</button>
+      <h4>4. ref记录上一次的状态</h4>
+      <p>count：{count}</p>
+      <p>stateRef：{stateRef.current}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }

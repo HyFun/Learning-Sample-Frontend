de37d147ca623e6a1568fb82e8f92250bfda318d
TD-Yofun <yongfeng.he@china.talkdesk.com>
2022-01-05 19:31:28 +0800

    chore: webpack vue initial

---
+++ Webpack/06-webpack-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">{{ message }}</div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+};
+</script>
+<style lang="sass" scoped>
+.app {
+    font-size: 21px;
+    background-color: purple;
+    color: white;
+}
+</style>

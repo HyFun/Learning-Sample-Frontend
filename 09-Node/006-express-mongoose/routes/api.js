cc75412fa94e606cd9dc9300dfeebec97d61defc
TD-Yofun <yongfeng.he@china.talkdesk.com>
2023-05-29 15:25:50 +0800

    feat(Node): todo list finish

--- 09-Node/006-express-mongoose/routes/api.js
+++ 09-Node/006-express-mongoose/routes/api.js
@@ -7,10 +7,14 @@ const TodoModel = require("../model/Todo");
 router.post("/register", async function (req, res, next) {
   const { username, password } = req.body;
   try {
+    const list = await UserModel.find({ username });
+    if (list.length) {
+      throw new Error("该用户已经存在");
+    }
     const data = await UserModel.create({ username, password });
     res.send({ ok: 1, data });
   } catch (error) {
-    res.send({ ok: 0 });
+    res.send({ ok: 0, errMsg: error.message });
   }
 });
 
@@ -27,6 +31,26 @@ router.post("/todo/add", async (req, res) => {
   try {
     // const { title, create_date, finish_date, done } = req.body;
     const data = await TodoModel.create(req.body);
+    res.send({ ok: 1, data });
+  } catch (error) {
+    res.send({ ok: 0 });
+  }
+});
+
+router.patch("/todo", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const data = await TodoModel.findByIdAndUpdate(id, req.body);
+    res.send({ ok: 1, data });
+  } catch (error) {
+    res.send({ ok: 0 });
+  }
+});
+
+router.delete("/todo", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const data = await TodoModel.findByIdAndDelete(id);
     res.send({ ok: 1, data });
   } catch (error) {
     res.send({ ok: 0 });

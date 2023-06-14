7a006076d399637a37ce2993af9d19405e97253f
TD-Yofun <yongfeng.he@china.talkdesk.com>
2023-06-14 15:10:04 +0800

    chore: test

--- 工程化/05-lerna-test/src/index.ts
+++ 工程化/05-lerna-test/src/index.ts
@@ -1,6 +1,12 @@
 import sum from "@test/sum";
 import decrease from "@test/decrease";
+import multiplication from "@test/multiplication";
+import division from "@test/division";
 
 console.log("sum", sum(1, 2, 3, +"4"));
 
 console.log("decrease", decrease(10, 1, 2, +"3"));
+
+console.log("multiplication", multiplication(1, 2, 3, +"4"));
+
+console.log("division", division(1000, 2));
